530f01aac749596126738263b9fa2302548f2875
shailendra-0 <shailendracheruku1611@gmail.com>
2023-08-17 15:48:58 +0530

    final commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="Footer">© All rights reserved to Debojeet Jha</div>;
+  return <div className="Footer">© All rights reserved to Shailendra Cheruku</div>;
 };
 
 export default Footer;
